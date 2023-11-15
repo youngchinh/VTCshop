9ef009c8f1bbf50353e75f5c1d9d0cc242c2f08e
chinhnb204 <nbc23082004>
2023-11-15 19:01:43 +0700

    rebuild src code

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,1 +1,25 @@
 
+<?php 
+    include "../views/Admin/header.php";
+    include "../views/Admin/sidebar.php";
+    if(isset($_GET['act'])&&($_GET['act']!="")){
+        $act=$_GET['act'];
+        switch($act) {
+            case "listdm";
+            include "../views/Admin/danhmuc/list.php";
+            break;
+
+            case "adddm";
+            include "../views/Admin/danhmuc/add.php";
+            break;
+        }
+
+    }
+
+
+    include "../views/Admin/footer.php";
+?>
+
+
+
+ 
--- app/views/Admin/sidebar.php
+++ app/views/Admin/sidebar.php
@@ -1,8 +1,8 @@
  <!-- Main Sidebar Container -->
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
      <!-- Brand Logo -->
-     <a href="index.php" class="brand-link">
-         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+     <a href="AdminController.php" class="brand-link">
+         <img src="../views/Admin/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
          <span class="brand-text font-weight-light">ADMIN</span>
      </a>
 
@@ -11,7 +11,7 @@
          <!-- Sidebar user panel (optional) -->
          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
              <div class="image">
-                 <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                 <img src="../views/Admin/assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
              </div>
              <div class="info">
                  <a href="#" class="d-block">Admin</a>
@@ -35,13 +35,13 @@
 
                      <ul class="nav nav-treeview">
                          <li class="nav-item">
-                             <a href="index.php?act=listdm" name= "ds-danhmuc" class="nav-link">
+                             <a href="AdminController.php?act=listdm" name= "ds-danhmuc" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Danh sách</p>
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="index.php?act=adddm" class="nav-link">
+                             <a href="AdminController.php?act=adddm" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Thêm Mới</p>
                              </a>
@@ -60,13 +60,13 @@
 
                      <ul class="nav nav-treeview">
                          <li class="nav-item">
-                             <a href="pages/tables/simple.html" class="nav-link">
+                             <a href="" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Danh sách</p>
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="pages/tables/simple.html" class="nav-link">
+                             <a href="../views/Admin/assets/pages/tables/simple.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Thêm Mới</p>
                              </a>
@@ -85,13 +85,13 @@
 
                      <ul class="nav nav-treeview">
                          <li class="nav-item">
-                             <a href="pages/tables/simple.html" class="nav-link">
+                             <a href="../views/Admin/assets/pages/tables/simple.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Danh sách</p>
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="pages/tables/simple.html" class="nav-link">
+                             <a href="../views/Admin/assets/pages/tables/simple.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Thêm Mới</p>
                              </a>
@@ -110,7 +110,7 @@
 
                      <ul class="nav nav-treeview">
                          <li class="nav-item">
-                             <a href="pages/tables/simple.html" class="nav-link">
+                             <a href="../views/Admin/assets/pages/tables/simple.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Danh sách</p>
                              </a>
@@ -129,13 +129,13 @@
 
                      <ul class="nav nav-treeview">
                          <li class="nav-item">
-                             <a href="pages/tables/simple.html" class="nav-link">
+                             <a href="../views/Admin/assets/pages/tables/simple.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Danh sách</p>
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="pages/tables/simple.html" class="nav-link">
+                             <a href="../views/Admin/assets/pages/tables/simple.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Thêm Mới</p>
                              </a>
@@ -154,13 +154,13 @@
 
                      <ul class="nav nav-treeview">
                          <li class="nav-item">
-                             <a href="pages/tables/simple.html" class="nav-link">
+                             <a href="../views/Admin/assets/pages/tables/simple.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Danh sách</p>
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="pages/tables/simple.html" class="nav-link">
+                             <a href="../views/Admin/assets/pages/tables/simple.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Thêm Mới</p>
                              </a>
@@ -179,7 +179,7 @@
 
                      <ul class="nav nav-treeview">
                          <li class="nav-item">
-                             <a href="pages/tables/simple.html" class="nav-link">
+                             <a href="../views/Admin/assets/pages/tables/simple.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Danh sách</p>
                              </a>
@@ -198,13 +198,13 @@
 
                      <ul class="nav nav-treeview">
                          <li class="nav-item">
-                             <a href="pages/tables/simple.html" class="nav-link">
+                             <a href="../views/Admin/assets/pages/tables/simple.html" class="nav-link">
                                  <i class="far fa-image nav-icon"></i>
                                  <p>Logo</p>
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="pages/tables/simple.html" class="nav-link">
+                             <a href="../views/Admin/assets/pages/tables/simple.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>footer</p>
                              </a>
