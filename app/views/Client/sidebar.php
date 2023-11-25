6bc880e4b6c8b6e2310da47768e544b41d9f256b
chinhnb204 <nbc23082004>
2023-11-25 23:39:48 +0700

    update src"

--- app/views/Client/sidebar.php
+++ app/views/Client/sidebar.php
@@ -3,7 +3,6 @@
     include "../views/Client/banner_1.php";
     include "../views/Client/sp_sale.php";
     include "../views/Client/banner_2.php";
-    include "../views/Client/top_danhmuc.php";
     include "../views/Client/sp_noibat.php";
     include "../views/Client/banner_3.php";
     include "../views/Client/bv_noibat.php";
